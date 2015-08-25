84b6031a5601216b9b2aed479eb50fc3283dd453
innokenty <innokenty@yandex-team.ru>
2015-08-25 18:20:31 +0300

    improve get.sh a tiny bit

--- get.sh
+++ get.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "//usage: sh get.sh <key (def. 'body1')> <host (def. 'localhost')> <port (def. 18081)>"
+echo "//usage: sh get.sh <key (def. 'body-1')> <host (def. 'localhost')> <port (def. 18081)>"
 
-key=${1:-'body1'}
+key=${1:-'body-1'}
 host=${2:-'localhost'}
 port=${3:-'18081'}
 
