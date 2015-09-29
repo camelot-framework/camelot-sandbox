1a6d25bce109d9a3026d837dae36c37dfe15fff7
Ilya Sadykov <smecsia@gmail.com>
2015-09-29 19:43:03 +0300

    Add example test

---
+++ src/test/java/sandbox/CollectorTest.java
@@ -0,0 +1,42 @@
+package sandbox;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ru.yandex.qatools.camelot.test.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Ilya Sadykov
+ */
+@RunWith(CamelotTestRunner.class)
+public class CollectorTest {
+    @PluginMock
+    Collector collector;
+
+    @AggregatorState(Collector.class)
+    AggregatorStateStorage storage;
+
+    @EndpointPluginInput(Collector.class)
+    MockEndpoint collectorEP;
+
+    @Helper
+    TestHelper helper;
+
+    @Test
+    public void testMessageIsReceivedByAggregator() throws InterruptedException {
+        collectorEP.expectedMessageCount(3);
+        helper.send("body1");
+        helper.send("body1");
+        helper.send("body1");
+        collectorEP.assertIsSatisfied(2000);
+        verify(collector, timeout(2000).times(3)).collect(any(State.class), eq("body1"));
+        assertThat(((State) storage.getActual("body1")).index, equalTo(3L));
+    }
+}
