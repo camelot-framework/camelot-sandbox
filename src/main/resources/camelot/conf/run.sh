39a97b5b404f05d8b616d4df598f5ec69d525fcf
Ilya Sadykov <smecsia@gmail.com>
2015-11-25 20:37:16 +0300

    cluster issues

--- src/main/resources/camelot/conf/run.sh
+++ src/main/resources/camelot/conf/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo $(getent hosts rabbitmq1.dc | awk '{ print $1 }')' rabbitmq1' >> /etc/hosts
 echo $(getent hosts rabbitmq2.dc | awk '{ print $1 }')' rabbitmq2' >> /etc/hosts
-sleep 10
+sleep 25
+mkdir -p /etc/camelot/log
 touch /etc/camelot/log/camelot-full.log
 /etc/camelot/bin/camelot.sh start
 tail -f /etc/camelot/log/camelot-full.log
--- src/main/resources/rabbitmq/run.sh
+++ src/main/resources/rabbitmq/run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+sleep 5
+
 echo $(getent hosts rabbitmq1.dc | awk '{ print $1 }')' rabbitmq1' >> /etc/hosts
 echo $(getent hosts rabbitmq2.dc | awk '{ print $1 }')' rabbitmq2' >> /etc/hosts
 
+if [ $(hostname) == "rabbitmq2" ]
+then
+    echo "Waiting while primary node warming up..."
+    sleep 10
+fi
 rabbitmq-server
